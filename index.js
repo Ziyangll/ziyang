c6ceb325400716a40b1b167d21f67d7e368c4015
Ziyang Li <ziyangli103@gmail.com>
2020-03-28 16:42:01 -0500

    todo list addes

--- index.js
+++ index.js
@@ -1,71 +1,89 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const ejs = require("ejs")
-const _ = require("lodash")
-
-app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+app = express();
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
-
-const homeContent = "jmnbhjknbhjknmbhjklnmbhjklmn,wedsfwd."
-let posts = [];
-
+// mongoose ----
+mongoose.connect("mongodb+srv://dbAdmin:dbAdmin@cluster0-tmzur.mongodb.net/todo?retryWrites=true&w=majority",
+	{
+		useUnifiedTopology: true, 
+		useNewUrlParser: true,
+		useFindAndModify: false
+	}
+);
+const itemSchema = {
+	name: String
+};
+const Item = mongoose.model("Item", itemSchema);
+const exampleItem0 = new Item({
+	name: "Welcome to your todo list"
+});
+const exampleItem1 = new Item({
+	name: "Created using Mongodb Atlas and Nodejs"
+});
+defaultItems = [exampleItem0, exampleItem1]
+// ----
+// date ----
+let today = new Date();
+today = today.toLocaleDateString("en-US", {weekday: "long", month: "long", day: "numeric"});
+// ----
+// server ----
 app.get("/", function(req, res) {
-	res.render("index")
+	res.render("index");
 });
+app.get("/todo", function(req, res) {
 
-app.get("/blog", function(req, res) {
-	res.render("blog", 
-		{
-			startingContent: homeContent,
-			posts: posts
+	Item.find({}, function(err, foundItems) {
+		if (err) {
+			console.log(err);
 		}
-	)
+		if (foundItems.length === 0) {
+			Item.insertMany(defaultItems ,function (err) {
+				if (err){
+					console.log(err);
+				} else {
+					console.log("Example items successfuly saved to database.");
+				}
+			});
+		}
+		res.render("list", {today:today, todoListItems: foundItems});
+	});
 });
-
+app.post("/todo",(req, res) => {
+	if (req.body.newItem.trim().length > 0) {
+	const aNewItem = new Item({
+		name: req.body.newItem
+	});
+	aNewItem.save(function (err) {
+		if (err) {
+			console.log(err);
+		}
+	})
+	}
+	res.redirect("/todo");
+});
+app.post("/delete", (req, res) => {
+	Item.findByIdAndRemove( req.body.checkbox, function(err) {
+		console.log(err);
+	});
+	res.redirect("/todo");
+});
 app.get("/resume", function(req, res) {
         res.render("resume")
 });
-
 app.get("/contact", function(req, res) {
         res.render("contact")
 });
-
 app.get("/projects", function(req, res) {
 	res.render("projects")
-});
-
-app.get("/compose-ziyang", function(req, res) {
-	res.render("compose")
-});
-
-app.post("/compose-ziyang", function(req, res) {
-	const post = {
-		title:req.body.newBlogTitle,
-		content:req.body.newBlogBody
-	};
-	posts.push(post)
-
-	res.redirect("/blog")
-});
-
-
-app.get("/posts/:title", function(req,res) {
-	let requestTitle = _.lowerCase(req.params.title);
-	
-	posts.forEach(function (post) {
-		if (_.lowerCase(post.title) === requestTitle) {
-			res.render("post", {
-				title:post.title,
-				body:post.content
-			});
-		}
-	});
-
 });
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+// ----
